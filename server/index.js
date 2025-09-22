311f738bbd50911aaf04a87395da3cb7be3e2116
Praphull Rahangdale <praphullrahangdale@gmail.com>
2025-09-22 12:40:57 +0530

    some Changes 2

--- server/index.js
+++ server/index.js
@@ -25,9 +25,7 @@ const limiter = rateLimit({
 app.use('/api/', limiter);
 
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-frontend-domain.vercel.app'] 
-    : ['http://localhost:5173'],
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
